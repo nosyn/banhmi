fd9493a06f8108365f512739ca9c9ff65f06916f
Son Nguyen <biem97@gmail.com>
2026-05-04 12:00:47 +0700

    feat(config): add @banhmi/config with ConfigService and ConfigModule.forRoot

---
+++ packages/config/src/config.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@banhmi/common'
+import { ConfigService, type EnvSchema } from './config.service'
+import { CONFIG_TOKEN } from './tokens'
+
+export class ConfigModule {
+  static forRoot<S extends EnvSchema>(
+    schema: S,
+    env?: Record<string, string | undefined>,
+  ) {
+    @Module({
+      providers: [
+        {
+          provide: CONFIG_TOKEN,
+          useFactory: () => new ConfigService(schema, env ?? Bun.env),
+        },
+      ],
+      exports: [CONFIG_TOKEN],
+    })
+    class ConfigRootModule {}
+
+    return ConfigRootModule
+  }
+}
---
+++ packages/config/src/tokens.ts
@@ -0,0 +1,4 @@
+import { Token } from '@banhmi/common'
+import type { ConfigService } from './config.service'
+
+export const CONFIG_TOKEN = Token<ConfigService>('ConfigService')
