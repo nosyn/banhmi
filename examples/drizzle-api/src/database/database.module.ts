5a9c362b4478fb8ad45e33d8e47d7e97301487c6
Son Nguyen <biem97@gmail.com>
2026-05-04 10:45:14 +0700

    feat(drizzle-api): add schema and DatabaseModule with DB_TOKEN

---
+++ examples/drizzle-api/src/database/database.module.ts
@@ -0,0 +1,22 @@
+import { Module, Token } from 'banhmi'
+import { Database } from 'bun:sqlite'
+import { drizzle } from 'drizzle-orm/bun-sqlite'
+import * as schema from './schema'
+
+export type DrizzleDB = ReturnType<typeof drizzle<typeof schema>>
+
+export const DB_TOKEN = Token<DrizzleDB>('DrizzleDB')
+
+@Module({
+  providers: [
+    {
+      provide: DB_TOKEN,
+      useFactory: (): DrizzleDB => {
+        const sqlite = new Database('./drizzle-api.sqlite')
+        return drizzle({ client: sqlite, schema })
+      },
+    },
+  ],
+  exports: [DB_TOKEN],
+})
+export class DatabaseModule {}
